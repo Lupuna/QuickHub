e9dcd6b1a120b5cd79d548762cd104017a9d7202
Lupuna <terragod427@gmail.com>
2023-11-07 13:33:28 +0700

    Create db

--- QuickHub/urls.py
+++ QuickHub/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('team/', include('team.urls'), namespace='team')
+    path('team/', include('team.urls', namespace='team'))
 ]
