be1551b325120eb9df215d10c8c6e5273a82b953
Lupuna <terragod427@gmail.com>
2023-12-25 13:25:56 +0700

    create from to sutask

--- team/forms.py
+++ team/forms.py
@@ -64,10 +64,22 @@ class TaskCreationForm(forms.Form):
 
     def __init__(self, company_id, project_id, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # получаем список тех, кто работает в компании
-        # возникла проблема: как создать связь с теми, кто работает над проектом? А нужна ли она вообще?
         self.employee_list = models.Employee.objects.filter(
             id__in=models.EmployeeCompany.objects.filter(company_id=company_id).values('employee_id'))
         self.fields['responsible'].queryset = self.employee_list
         self.fields['executor'].queryset = self.employee_list
         self.fields['parent_id'].queryset = models.Project.objects.get(id=project_id).task_set.all()
+
+
+class SubtaskCreationForm(forms.Form):
+    title = forms.CharField(max_length=40)
+    images = MultipleImageField(required=False)
+    text = forms.CharField(widget=forms.Textarea, required=False)
+    files = MultipleFileField(required=False)
+    responsible = forms.ModelMultipleChoiceField(queryset=None, widget=forms.CheckboxSelectMultiple)
+    executor = forms.ModelMultipleChoiceField(queryset=None, widget=forms.CheckboxSelectMultiple, required=False)
+
+    def __init__(self, company_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.employee_list = models.Employee.objects.filter(
+            id__in=models.EmployeeCompany.objects.filter(company_id=company_id).values('employee_id'))
