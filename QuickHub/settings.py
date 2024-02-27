855f631ce92e0f986ba145c77ca40b1686b2fd33
Lupuna <terragod427@gmail.com>
2024-02-27 18:51:36 +0700

    update dbug-tools

--- QuickHub/settings.py
+++ QuickHub/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     # 'channels',
     'team.apps.TeamConfig',
 ]
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'QuickHub.urls'
@@ -80,6 +81,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'QuickHub.wsgi.application'
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
--- QuickHub/urls.py
+++ QuickHub/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('team/', include('team.urls', namespace='team')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
