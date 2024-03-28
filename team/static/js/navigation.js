827b4927658af565f54e57d20f75c8da90cdfe20
AlekseySimonov <aleksey.simonov04@mail.ru>
2024-03-28 16:10:49 +0300

    base and left_bar animation

--- team/static/js/navigation.js
+++ team/static/js/navigation.js
@@ -4,38 +4,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelectorAll('.nav_link')
     const loadingOverlay = document.querySelector('.loading_overlay')
 
-
-    console.log(navLinks)
-    console.log(loadingOverlay)
-    const ShowLoadingOverlay = () => {
+    const showLoadingOverlay = () => {
         loadingOverlay.style.opacity = '1'
     }
 
-    const HideLoadingOverlay = () => {
+    const hideLoadingOverlay = () => {
         loadingOverlay.style.opacity = '0'
     }
 
     const loadPage = (url) => {
-        ShowLoadingOverlay()
+        showLoadingOverlay()
         fetch(url)
             .then(response => response.text())
 
             .then(html => {
                 const parser = new DOMParser()
                 const doc = parser.parseFromString(html, 'text/html')
-                const newContent = doc.querySelector('content').innerHTML
+                const newContent = doc.querySelector('.content').innerHTML
 
-                contentDiv.classList.add('fade-out');
+                contentDiv.classList.add('fade_out');
+                
 
                 contentDiv.innerHTML = newContent;
                 document.title = doc.title
 
                 setTimeout(() => {
-                    contentDiv.classList.remove('fade-out')
+                    contentDiv.classList.remove('fade_out')
                     history.pushState({}, '', url)
-                    HideLoadingOverlay()
+                    hideLoadingOverlay()
                 }, 500)
-            
+
             })
     }
 
@@ -44,14 +42,17 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault()
 
             const url = e.currentTarget.getAttribute('href')
+            showLoadingOverlay()
             loadPage(url)
         })
     })
 
-    
+
     loadPage(window.location.pathname)
 
     window.addEventListener('popstate', () => {
         loadPage(window.location.pathname)
     })
+
 })
+
