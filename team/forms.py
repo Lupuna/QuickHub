46498741d5f954884ed361b1c7694ab55830176b
Lupuna <terragod427@gmail.com>
2024-04-06 12:27:36 +0700

    complete test coverage

--- team/forms.py
+++ team/forms.py
@@ -20,6 +20,7 @@ class CompanyCreationForm(forms.ModelForm):
 
 
 class ChoiceEmployeeParametersForm(forms.Form):
+    # аккуратно! для этой формы любая информация в любом виде будет валидна
     image = forms.BooleanField(required=False, initial=True)
     name = forms.BooleanField(required=False, initial=True)
     email = forms.BooleanField(required=False, initial=True)
@@ -69,7 +70,6 @@ class CompanyEventCreationForm(forms.Form):
 
     def __init__(self, company_id, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self.fields['present_employees'].queryset = utils.create_employee_list(company_id=company_id)
         self.fields['present_employees'].queryset = company_id.employees.distinct()
 
     # Валидатор кастомный
